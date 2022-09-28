46dd244e59e2c918dc44d4ea4dd18d7d4b44bfb5
Waqar-Hafeez-Ahmed <you@example.com>
2022-09-28 18:42:06 +0500

    Keyboard dark mode set

--- keyboard.js
+++ keyboard.js
@@ -6,7 +6,7 @@ let caps_lock_key = document.querySelector(".caps_lock_key");
 let toggle_circle = document.querySelector(".toggle_circle");
 let night_mode = document.querySelector(".night_mode");
 let body = document.querySelector("body");
-let text_input = document.querySelector(".text");
+// let text_input = document.querySelector(".text");
 let change_color = document.querySelector(".change_light_color");
 let colors_input = document.querySelector(".colors_input");
 let keyboard_lights = document.querySelector(".keyboard_lights");
@@ -72,7 +72,7 @@ night_mode.addEventListener("click", function () {
   body.classList.toggle("active");
   night_mode.classList.toggle("active");
   keyboard_wrapp.classList.toggle("active");
-  text_input.classList.toggle("active");
+  // text_input.classList.toggle("active");
   change_color.classList.toggle("active");
   for (let i = 0; i < keys.length; i++) {
     keys[i].classList.toggle("keys_night");
