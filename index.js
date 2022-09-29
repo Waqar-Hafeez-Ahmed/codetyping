63701356af606c338401b893c51fd17264ba8f56
Waqar Ahmed <waqarhafeez12@gmail.com>
2022-09-29 18:23:42 +0500

    Synced keys, Activated first keys, remove error count on enter key

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var svg = `
 `;
 
 let code = `
-       11234567;
+       let var = 10;
       console.log(a, "HELLO");`;
 
 const lines = code.split("\n");
@@ -41,9 +41,18 @@ document.body.appendChild(element);
 
 var idx = 0;
 var letters = document.getElementsByClassName("letter");
-var keys = document.getElementsByClassName("keys");
 var interval;
 
+window.addEventListener("load", function firstKey() {
+  const element = document.querySelector(
+    `[keyname="${letters[idx].innerText.toUpperCase()}"]`
+  );
+  element.className += " active";
+});
+
+if (idx === 0) {
+  letters[idx].className += " current";
+}
 window.addEventListener("keypress", function onKeyPress(e) {
   var correct = false;
   if (interval === undefined) {
@@ -52,7 +61,6 @@ window.addEventListener("keypress", function onKeyPress(e) {
       time.innerHTML = Number(time.innerHTML) + 1;
     }, 1000);
   }
-
   if (
     letters[idx].className == "letter return" ||
     letters[idx].className == "letter return current"
@@ -62,32 +70,24 @@ window.addEventListener("keypress", function onKeyPress(e) {
     }
   }
 
-  // next.classList.remove("active");
-  // if (e.code == "Space") {
-  //   let spaceKey = document.querySelector(".space_key");
-  //   spaceKey.classList.add("active");
-  //   return;
-  // }
-  var next = document.getElementsByClassName("letter");
-  console.log(next[idx].innerText);
+  activeKey(letters[idx + 1]);
   letters[idx + 1].className += " current";
-  keys[idx + 1].className += " active";
   letters[idx].className = letters[idx].className.replace(" current", "");
-  keys[idx].className = keys[idx].className.replace(" active", " remove");
-
-  // console.log(next);
-  // if (next) next.classList.add("active");
   if (letters[idx].innerHTML !== String.fromCharCode(e.which)) {
     letters[idx].className += " wrong";
   } else {
     correct = true;
     letters[idx].className += " correct";
   }
-  document.getElementsByClassName("errors")[0].innerHTML = Array.prototype.slice
-    .call(document.getElementsByClassName("wrong"), 0)
-    .reduce(function (a, b) {
-      return a + 1;
-    }, 0);
+
+  if (e.key !== "Enter") {
+    document.getElementsByClassName("errors")[0].innerHTML =
+      Array.prototype.slice
+        .call(document.getElementsByClassName("wrong"), 0)
+        .reduce(function (a, b) {
+          return a + 1;
+        }, 0);
+  }
   idx++;
 
   if (idx + 1 >= letters.length) {
@@ -95,3 +95,25 @@ window.addEventListener("keypress", function onKeyPress(e) {
     window.clearInterval(interval);
   }
 });
+
+function activeKey(nextChar) {
+  // console.log(event.code);
+  const element =
+    nextChar.innerText !== " "
+      ? document.querySelector(
+          `[keyname="${nextChar.innerText.toUpperCase()}"]`
+        )
+      : document.querySelector(`[keyname=""]`);
+
+  element.className += " active";
+}
+
+// function firstKey(key) {
+//   // const element = key.innerText.toUpperCase();
+//   const element = document.querySelector(
+//     `[keyname="${key.innerText.toUpperCase()}"]`
+//   );
+
+//   console.log(key);
+//   // element.className += " active";
+// }
